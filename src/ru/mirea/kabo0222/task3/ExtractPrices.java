9d6fe712e87d1cf0045dab7bee1ae3e3a663d7e7
Takhirov Aykhan <144058450+23a-takhirov@users.noreply.github.com>
2023-10-29 15:16:27 +0300

    ExtractPrices.java

--- src/ru/mirea/kabo0222/task3/ExtractPrices.java
+++ src/ru/mirea/kabo0222/task3/ExtractPrices.java
@@ -4,19 +4,14 @@ import java.util.regex.Pattern;
 
 public class ExtractPrices {
     public static void main(String[] args) {
-        // Входной текст со списками цен
         String inputText = "Примеры цен: 25.98 USD, 100 RUB, 10.50 EU, 44 ERR, 0.004 EU.";
 
-        // Регулярное выражение для поиска цен в USD, RUB и EUR
         String regex = "(\\d+(\\.\\d{2})?)\\s+(USD|RUB|EU)";
 
-        // Создание объекта Pattern для компиляции регулярного выражения
         Pattern pattern = Pattern.compile(regex);
 
-        // Создание объекта Matcher для поиска соответствий в тексте
         Matcher matcher = pattern.matcher(inputText);
 
-        // Извлечение и вывод цен по найденным соответствиям
         System.out.println("Извлеченные цены:");
         while (matcher.find()) {
             String price = matcher.group(1);
